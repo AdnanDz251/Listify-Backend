6ca3d72d57bd8775afbb3d84e6e05b626fa807f3
AdnanDz251 <adnan.dzindo1507@gmail.com>
2023-12-23 14:12:25 +0100

    inital commit

---
+++ dbConnect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.DB_URI, {   
+})
+    .then(() => console.log("Connected to DB"))
+    .catch(console.error);
