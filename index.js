c0b7532501b87b250e31533cbc549e7766f766ea
AdnanDz251 <adnan.dzindo1507@gmail.com>
2024-01-09 13:45:31 +0100

    fixed a bug

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.all('*', authJWT.verifyUserToken);
 
